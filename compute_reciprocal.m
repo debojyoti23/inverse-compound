55e26cc6a22cb7960e3fb935e3dcd413091d2bb4
Debojyoti Dey <debuiem@gmail.com>
2026-03-28 20:56:14 +0300

    reciprocal of kth compound computation

---
+++ compute_reciprocal.m
@@ -0,0 +1,13 @@
+function [Uk_rcp, k_out] = compute_reciprocal(Uk,n,k)
+    % this function computes C_k(U) from given C_{n-k}(U) where U is
+    % non-singular square matrix and full rank using the formula C_k(A) =
+    % (detA)^(-1) SP(C_{n-k}^T)^{-1} PS
+
+    C = nchoosek(1:n,k);
+    P = fliplr(eye(size(Uk)));
+    S = diag((-1).^sum(C,2));
+    detU = nthroot(det(Uk),nchoosek(n-1,k-1));
+    Uk_rcp = detU*S*P*(Uk'\eye(size(Uk')))*P*S;
+    k_out = n-k;
+end
+
